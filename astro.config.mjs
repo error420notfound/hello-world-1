c5365bb0cd686af73f6587aea572749af5f1c50a
error420notfound <firers-bustard0d@icloud.com>
2026-03-27 00:16:14 +0530

    some updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,8 +6,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://username.github.io', // Replace 'username' with your GitHub username
-  base: '/midnight-pulse', // Replace 'midnight-pulse' with your repository name
+  site: 'https://error420notfound.github.io', // Replace 'username' with your GitHub username
+  base: '/hello-world-1', // Replace 'midnight-pulse' with your repository name
   integrations: [react()],
 
   vite: {
